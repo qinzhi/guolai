3627fa9812463de606714174019b2cbefd12d4fc
qinzhi <631248045@qq.com>
2016-03-30 22:11:19 +0800

    添加商品

--- Application/Admin/Tpl/Default/Goods/Add/base.php
+++ Application/Admin/Tpl/Default/Goods/Add/base.php
@@ -57,10 +57,10 @@
                         <tr>
                             <th class="base">商品货号</th>
                             <th class="base">库存</th>
-                            <th class="base">市场价格</th>
-                            <th class="base">销售价格</th>
-                            <th class="base">成本价格</th>
-                            <th class="base">重量(克)</th>
+                            <th class="base">重量(千克)</th>
+                            <!--<th class="base">购买成功增加积分</th>-->
+                            <th class="base">排序</th>
+                            <th class="base">计量单位显示</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -77,22 +77,22 @@
                             </td>
                             <td class="base">
                                 <div class="form-group has-feedback no-margin">
-                                    <input class="input-xs Lwidth80" type="text" name="_market_price[]" pattern="float" maxlength="10">
-                                </div>
-                            </td>
-                            <td class="base">
-                                <div class="form-group has-feedback no-margin">
-                                    <input class="input-xs Lwidth80" type="text" name="_sell_price[]" pattern="float" maxlength="10">
-                                </div>
-                            </td>
-                            <td class="base">
-                                <div class="form-group has-feedback no-margin">
-                                    <input class="input-xs Lwidth80" type="text" name="_cost_price[]" pattern="float" maxlength="10">
-                                </div>
-                            </td>
-                            <td class="base">
-                                <div class="form-group has-feedback no-margin">
                                     <input class="input-xs Lwidth80" type="text" name="_weight[]" pattern="float" maxlength="10">
+                                </div>
+                            </td>
+                            <!--<td class="base">
+                                <div class="form-group has-feedback no-margin">
+                                    <input class="input-xs Lwidth80" type="text" name="_point[]" pattern="float" maxlength="10">
+                                </div>
+                            </td>-->
+                            <td class="base">
+                                <div class="form-group has-feedback no-margin">
+                                    <input class="input-xs Lwidth80" type="text" name="_sort[]" pattern="float" maxlength="10">
+                                </div>
+                            </td>
+                            <td class="base">
+                                <div class="form-group has-feedback no-margin">
+                                    <input class="input-xs Lwidth80" type="text" name="_unit[]" pattern="float" maxlength="10">
                                 </div>
                             </td>
                         </tr>
@@ -101,13 +101,46 @@
             </td>
         </tr>
         <tr>
-            <th>商品规格：</th>
+            <th>批发价格：</th>
+            <td>
+                <table class="table table-bordered table-hover table-border">
+                    <thead>
+                        <tr>
+                            <th class="base">购买件数</th>
+                            <th class="base">平台价格</th>
+                            <th class="base">成本价格</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="base">
+                            <td class="base">
+                                <div class="form-group has-feedback no-margin">
+                                    <span>≥</span>
+                                    <input class="input-xs Lwidth80" type="text" name="_goods_no[]" pattern="required" maxlength="20">
+                                </div>
+                            </td>
+                            <td class="base">
+                                <div class="form-group has-feedback no-margin">
+                                    <input class="input-xs Lwidth80" type="text" name="_market_price[]" pattern="float" maxlength="10">
+                                </div>
+                            </td>
+                            <td class="base">
+                                <div class="form-group has-feedback no-margin">
+                                    <input class="input-xs Lwidth80" type="text" name="_cost_price[]" pattern="float" maxlength="10">
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+        <tr>
+            <th>批发规则：</th>
             <td>
                 <a class="btn btn-success btn-sm pull-left no-radius" href="javascript:void(0);" id="addSpec" data-status="true">
                     <i class="fa fa-plus"></i>
-                    添加规格
+                    添加规则
                 </a>
-                <span class="note margin-left-10 pull-left margin-top-4">可以生成不同组合参数的货品，比如：尺码xxl+红色+长袖 衣服</span>
             </td>
         </tr>
         <tr>
@@ -143,168 +176,6 @@
 </table>
 <script>
     $(function(){
-        var goods_base = '';
-        $('#addSpec').click(function(){
-            if($(this).data('status') == false){
-                Notify('重新设置规格需删除当前规格列表', 'bottom-right', '5000', 'warning', 'fa-warning', true);
-                return;
-            }
-            $.dialog({
-                id : 'setSpec',
-                title : '设置商品规格',
-                async : false,
-                min_width: 600,
-                min_height: 350,
-                content : function(){
-                    var content;
-                    $.post("{:U('Goods/spec')}",{tpl:'set'},function(data){
-                        content = data;
-                    });
-                    return content;
-                },
-                ok : function(target){
-                    var spec = [];
-                    var table = $('.table-border');
-                    var thead = table.find('thead');
-                    var tbody = table.find('tbody');
-                    var tabs_spec = $(target).find('.tabs-spec-name li');
-                    if(tabs_spec.length){
-                        tabs_spec.each(function(index){
-                            var id = $(this).data('id');
-                            var name = $(this).data('name');
-                            var type = $(this).data('type');
-                            spec[index] = [];
-                            var tr = $(target).find('div[data-id='+id+']').find('table > tbody > tr');
-                            if(tr.length > 0){
-                                tr.each(function(i){
-                                    spec[index][i] = $(this).data('value');
-                                });
-                                spec[index]['name'] = name;
-                                spec[index]['type'] = type;
-                                spec[index]['id'] = id;
-                            }else{
-                                spec.pop();
-                            }
-                        });
 
-                        var spec_depth = spec.length;
-                        if(spec_depth > 0){
-
-                            if(goods_base == ''){
-                                goods_base = tbody.find('tr.base');
-                                tbody.find('tr.base').remove();
-                            }
-
-                            var th = table.find('thead > tr');
-                            var _spec = [];
-                            $.each(spec,function(i){
-                                var tmp = $('<th class="spec">' + this.name + '</th>');
-                                tmp.data('id',this.id)
-                                    .data('name',this.name)
-                                    .data('type',this.type);
-                                _spec[i] = {};
-                                _spec[i].id = this.id;
-                                _spec[i].name = this.name;
-                                _spec[i].type = this.type;
-                                th.prepend(tmp);
-                                delete this.type;
-                                delete this.name;
-                                delete this.id;
-                            });
-
-                            th.append('<th class="spec">默认</th>');
-                            th.append('<th class="spec">操作</th>');
-
-                            var spec_list = Zuhe(spec);
-
-                            for(var i in spec_list){
-
-                                var tr = $('<tr class="spec"></tr>');
-                                var list = spec_list[i].split(',');
-                                for(var l in list){
-                                    var td = $('<td data-value="' + list[l] + '">' + list[l] + '</td>');
-                                    tr.append(td);
-                                    var index = td.index();
-                                    var _input = _spec[index];
-                                    _input.value = list[l];
-                                    td.append("<input type='hidden' value='" + JSON.stringify(_input) + "' name='_spec_list[" + i + "][]'/>");
-                                }
-
-                                var base_tr = goods_base.find('td').clone().removeAttr('class');
-                                base_tr.find('.has-error').removeClass('has-error');
-                                tr.append(base_tr);
-
-                                var td = $('<td></td>');
-                                td.append('<div class="checkbox no-margin"></div>');
-                                td.find('div').append('<label class="no-padding-left"></label>');
-
-                                var _checkbox = $('<input type="checkbox" name="_default[]" value="' + i + '" class="inverted" />');
-                                if(i == 0) _checkbox.attr('checked',true);
-                                td.find('div > label').append(_checkbox);
-
-                                td.find('div > label').append('<span class="text margin-left-4"></span>');
-
-                                tr.append(td);
-
-                                tr.append('<td><a href="javascript:void(0);" class="btn btn-danger btn-xs shiny icon-only white btn-del"><i class="fa fa-times"></i></a></td>');
-                                tbody.append(tr);
-                            }
-
-                            $('#addSpec').data('status',false);
-
-                            tbody.find('input[name="default[]"]').click(function(){
-                                if(this.checked === false){
-                                    if(tbody.find('input[name="default[]"]:checked').length <= 0){
-                                        this.checked = true;
-                                    }
-                                }else{
-                                    var checkbox = tbody.find('input[name="default[]"]:checked');
-                                    if(checkbox.length > 0){
-                                        checkbox.attr('checked',false);
-                                    }
-                                    this.checked = true;
-                                }
-                            });
-
-                            tbody.find('.btn-del').click(function(){
-                                var spec_td = $(this).closest('tbody').find('tr.spec');
-                                if(spec_td.length == 1){
-                                    th.find('th.spec').remove();
-                                    tbody.html(goods_base);
-                                    $('#addSpec').data('status',true);
-                                }else{
-                                    var tr = $(this).closest('tr');
-                                    var checkbox = tr.find('input[type="checkbox"]').get(0);
-                                    if(checkbox.checked == true){
-                                        tr.remove();
-                                        tbody.find('tr:first-child').find('input[type="checkbox"]').attr('checked',true);
-                                    }
-                                }
-                            });
-                        }
-                    }
-                }
-            });
-        });
     });
-    //接受可变长数组参数，具体看下面的测试代码
-    function Zuhe(spec){
-        var heads=spec[0];
-        for(var i=1,len=spec.length;i<len;i++){
-            heads=addNewType(heads,spec[i]);
-        }
-        return heads;
-    }
-
-    //在原有组合结果的基础上添加一种新的规格
-    function addNewType(heads,choices){
-        var result=[];
-        for(var i=0,len=heads.length;i<len;i++){
-            for(var j=0,lenj=choices.length;j<lenj;j++){
-                result.push(heads[i]+','+choices[j]);
-            }
-        }
-        return result;
-    }
-
 </script>
